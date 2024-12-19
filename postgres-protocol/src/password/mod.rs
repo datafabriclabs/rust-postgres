0d9fab501eb320ba39871ea6d2e8a172bfedd102
xhwhis <hi@whis.me>
2024-12-19 14:13:45 +0800

    feat: support sm3 auth

--- postgres-protocol/src/password/mod.rs
+++ postgres-protocol/src/password/mod.rs
@@ -14,6 +14,7 @@ use md5::Md5;
 use rand::RngCore;
 use sha2::digest::FixedOutput;
 use sha2::{Digest, Sha256};
+use sm3::Sm3;
 
 #[cfg(test)]
 mod test;
@@ -103,4 +104,19 @@ pub fn md5(password: &[u8], username: &str) -> String {
     hash.update(&salted_password);
     let digest = hash.finalize();
     format!("md5{digest:x}")
+}
+
+/// Hash password using SM3 with the username as the salt.
+///
+/// The client may assume the returned string doesn't contain any
+/// special characters that would require escaping.
+pub fn sm3(password: &[u8], username: &str) -> String {
+    // salt password with username
+    let mut salted_password = Vec::from(password);
+    salted_password.extend_from_slice(username.as_bytes());
+
+    let mut hash = Sm3::new();
+    hash.update(&salted_password);
+    let digest = hash.finalize();
+    format!("sm3{:x}", digest)
 }
